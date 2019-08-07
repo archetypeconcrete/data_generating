115a88e8b4eb45e620b12ecf968985acf8a7937a
galyaveev.n <990@milandr.ru>
2019-08-07 17:16:44 +0300

    last ver.

--- highs_lows.py
+++ highs_lows.py
@@ -1,4 +1,6 @@
 import csv
+from datetime import datetime
+from matplotlib import pyplot as plt
 
 # Чтение максимальных температур из файла.
 filename = 'sitka_weather_07-2014.csv'
@@ -6,8 +8,31 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs = []
+    dates, highs, lows = [], [], []
     for row in reader:
-        highs.append(row[1])
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
 
-    print(highs)
+        high = int(row[1])
+        highs.append(high)
+
+        low = int(row[3])
+        lows.append(low)
+
+# Нанесение данных на диаграмму.
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
+
+# Форматирование диаграммы.
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
